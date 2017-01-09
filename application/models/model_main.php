cbcc8714799194bec619897b90fe7e8afb9e48c7
grigoryMovchan <grigory08@ya.ru>
2017-01-09 23:28:23 +0300

    changed model

--- application/models/model_main.php
+++ application/models/model_main.php
@@ -7,26 +7,7 @@
 
     public function get_posts()
     {
-      $db_login = $this->get_config('mysql');
-
-      $mysqli = new mysqli($db_login['host'], $db_login['user'],
-      $db_login['password'], $db_login['db']);
-
-      if ($mysqli->connect_errno) {
-      echo 'Ошибка соединения с БД \n"';
-      echo "Номер_ошибки: " . $mysqli->errno . "<br>";
-      echo "Ошибка: " . $mysqli->error . "<br>";
-      $mysqli->close();
-      }
-
-      /*принудительно установил кодировку UTF-8 потому что иначе MYSQL не понимала
-      со словами на русском языке*/
-      if (!$mysqli->set_charset("utf8")) {
-      /*printf("Ошибка при загрузке набора символов utf8: %s\n", $mysqli->error);*/
-      $mysqli->close();
-      } else {
-      /*printf("Текущий набор символов: %s\n", $mysqli->character_set_name());*/
-      }
+      $mysqli = $this->init_mysql_connection();
 
       $sql = 'SELECT * FROM `posts` ORDER BY `posts`.`date` DESC';
 
